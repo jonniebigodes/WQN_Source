ef01042a8bbdc61036849ed1b5331155c7dad45c
Doomd <bcosteloe@gmail.com>
2019-12-19 20:29:08 -0700

    Added a bunch of custom javascript functions to form page...not sure if any of it works yet

--- src/components/bcform.js
+++ src/components/bcform.js
@@ -2,6 +2,138 @@ import React from 'react'
 import {NormalField} from 'reactstrap-form-fields'
 import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
+// BC's Scripts below
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwbrIjWVJfPDC4AZGHmopV3sDXDRvrZ7BniEVP2shUn0EjJDFV9/exec'
+const scriptURLBad = 'https://script.google.com/macros/s/poop/exec' //Not a Real URL, used to test errors in connection
+const form = document.forms['submit-to-google-sheet']
+const contactPref = form.elements["contactMethod"];
+const loading = document.querySelector('.js-loading')
+const callout = document.querySelector('.callout')
+const callouts = document.querySelectorAll('.bs-callout') //All the Callouts
+const invalidMessage = document.querySelector('.bs-callout-warning')
+const validMessage = document.querySelector('.bs-callout-info')
+const successMessage = document.querySelector('.bs-callout-success')
+const errorMessage = document.querySelector('.bs-callout-error')
+const processingMessage = document.querySelector('.bs-callout-processing')
+const thanks = document.querySelector('.thanks')
+const thanksOL = document.querySelector('.form-success-overlay')
+
+const main = document.querySelector(".main")
+const preload = document.querySelector(".loadingScreen")
+
+function loadingScreen () {
+    preload.classList.add('hidden');
+    main.classList.remove('hidden');
+}
+
+//BC's OG Scripts:
+//Function used to turn on/off visibility and requirement of form elements
+
+function require(parentDiv,target, req) {
+    req == true ? text = "Required" : text = "Not Required"
+    //alert("The " + target + " is now " + text);
+    t = document.getElementById(target);
+    pD = document.getElementById(parentDiv)
+    //req ? pD.style.display = "block" : pD.style.display = "none";
+    req ? (t.setAttribute("required", ""), t.required = true, pD.style.display = "block") : (t.removeAttribute("required"), t.required = false, pD.style.display = "none");
+    $(form).parsley().validate();
+}
+
+//Normalize Phone Number after allowing liberal input of US Phone Numbers:
+function normalizePhone(phoneInput) {
+    var phoneNum = phoneInput.value;
+    var phoneRegex = /^([+]?[1]?[-.\s]?)([(]?)([2-9]\d{2})([)]?[-.\s]?)(\d{3})([-.\s]?)(\d{4})$/;
+    if (phoneRegex.test(phoneNum)) {
+        var normalizedNum = phoneNum.replace(phoneRegex, "$3-$5-$7");
+        //alert("The formatted Phone Number is: " + normalizedNum);
+        phoneInput.value = normalizedNum;
+    } else {
+        //alert("phone number wasn't valid! (" + normalizedNum + ")");
+    }
+}
+
+function testFuck() {
+    alert("fuck you");
+}
+//SUBMIT FORM
+function changeSubmit(message,disable) {
+    const button = document.getElementById('submit');
+    disable ? button.disabled = true : button.disabled = false;
+    button.innerHTML = message;
+}
+
+function hideAllMessages() {
+    invalidMessage.classList.add('hidden')
+    validMessage.classList.add('hidden')
+    successMessage.classList.add('hidden')
+    errorMessage.classList.add('hidden')
+    processingMessage.classList.add('hidden')
+}
+
+function success(response) {
+    console.log('Success!', response);
+    changeSubmit("It Worked!",true);
+    //alert("It fucking worked!");
+    setTimeout(() => {
+        hideAllMessages();
+        main.classList.add('hidden')
+        successMessage.classList.remove('hidden')
+        thanksOL.classList.remove('hidden')
+        //loading.classList.add('hidden')
+    }, 500)
+}
+
+function fuckup(error) {
+    console.error('Error!', error.message);
+    changeSubmit("Try Again",false);
+    //alert("Something Fucked up");
+    setTimeout(() => {
+        hideAllMessages();
+        errorMessage.classList.remove('hidden')
+        //loading.classList.add('hidden')
+    }, 500)
+}
+
+function thankYou() {
+    location.href = "thanks.html";
+} 
+
+//OG Form Scripts
+function showLoadingIndicator () {
+    form.classList.add('hidden')
+    loading.classList.remove('hidden')
+}
+
+//Parsley Scripts
+$(function () {
+    $(form).parsley().on('field:validated', function () {
+        var ok = $('.parsley-error').length === 0;
+        callout.classList.remove('hidden');
+        $('.bs-callout-info').toggleClass('hidden', !ok);
+        $('.bs-callout-warning').toggleClass('hidden', ok);
+        successMessage.classList.add('hidden');
+        errorMessage.classList.add('hidden');
+    })
+    .on('form:submit', function () {
+        //return false; // Don't submit form for this demo
+        changeSubmit("Processing...", true);
+        $(form).parsley().validate();
+        if ($(form).parsley().isValid())  {
+            hideAllMessages();
+            processingMessage.classList.remove('hidden');
+            //alert("Form Data is Valid! Let's POST the Data");
+            fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => success(response))
+            .catch(error => fuckup(error))
+        }
+        // prevent default so the form doesn't submit. We can return true and
+        // the form will be submited or proceed with a ajax request.
+        event.preventDefault();
+    })
+});
+
+
+
 export default () => (
     <form>
         <Row form>
