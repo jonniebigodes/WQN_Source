18c94c33e5f3fa4a2550b856531a9fbdc0cadeb1
Doomd <bcosteloe@gmail.com>
2019-12-21 21:29:36 -0700

    Got the Form to Submit Properly to Google Sheets!

--- src/components/bcformfuck.js
+++ src/components/bcformfuck.js
@@ -45,9 +45,152 @@ export default function App() {
     return (
 
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input name="firstName" ref={register} />
-            <input name="lastName" ref={register} />
-            <button type="submit">Submit</button>
-        </form>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-2 control-label" for="email" hidden>Email</label>
+                    <div class="col">
+                        <input id="email" name="email" type="email" placeholder="your@emailaddress.com"
+                            class="form-control input-md" data-parsley-trigger="change" ref={register({required:true})} />
+
+                    </div>
+                </div>
+            </div>
+        
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-2 control-label" for="firstName" hidden>First Name</label>
+                    <div class="col">
+                        <input id="firstName" name="firstName" type="text" placeholder="First Name"
+                            class="form-control input-md" ref={register({required:true})}/>
+
+                    </div>
+                    <label class="col-2 control-label" for="lastName">Last Name</label>
+                    <div class="col">
+                        <input id="lastName" name="lastName" type="text" placeholder="Last Name" class="form-control input-md"
+                            ref={register({required:true})}/>
+
+                    </div>
+                </div>
+            </div>
+        
+            <div class="form-group">
+                <div class="row">
+                    <label class="col control-label" for="vehicles">How many vehicles do you own?</label>
+                    <div class="col" id="error_vehicles">
+                        <label class="radio-inline" for="vehicles-0">
+                            <input type="radio" name="vehicles" id="vehicles-0" value="1" ref={register({required:true})}data-parsley-errors-container="#error_vehicles" /> 1 &nbsp;&nbsp;
+                        </label>
+                        <label class="radio-inline" for="vehicles-1">
+                            <input type="radio" name="vehicles" id="vehicles-1" value="2" ref={register({required:true})}/> 2 &nbsp;&nbsp;
+                        </label>
+                        <label class="radio-inline" for="vehicles-2">
+                            <input type="radio" name="vehicles" id="vehicles-2" value="3" ref={register({required:true})}/> 3 &nbsp;&nbsp;
+                        </label>
+                        <label class="radio-inline" for="vehicles-3">
+                            <input type="radio" name="vehicles" id="vehicles-3" value="4" ref={register({required:true})}/> 4 &nbsp;&nbsp;
+                        </label>
+                        <label class="radio-inline" for="vehicles-4">
+                            <input type="radio" name="vehicles" id="vehicles-4" value="5" ref={register({required:true})}/> 5+
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <label class="col control-label" for="insured">Are you currently insured?</label>
+                    <div class="col" id="insured_error">
+                        <label class="radio-inline" for="insured-0">
+                            <input type="radio" name="insured" id="insured-0" value="Yes" ref={register({required:true})}/> I'm Currently Insured
+                        </label>
+                        <label class="radio-inline" for="insured-1">
+                            <input type="radio" name="insured" id="insured-1" value="No" ref={register({required:true})}/> I'm NOT Insured
+                        </label>
+                    </div>
+                </div>
+            </div>
+        
+
+            <div class="form-group">
+                <div class="row">
+                    <label class="control-label col" for="interest">How can we help you?</label>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label class="radio-inline" for="interest-0">
+                            <input type="radio" name="interest" id="interest-0" value="save money" ref={register({required:true})}/>
+                            I want to save money on insurance.
+                        </label>
+                    </div>
+                    <div class="col">
+                        <label class="radio-inline" for="interest-1">
+                            <input type="radio" name="interest" id="interest-1" value="better insurance" ref={register({required:true})}/>
+                            I want better insurance.
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label class="radio-inline" for="interest-2">
+                            <input type="radio" name="interest" id="interest-2" value="personalized service" ref={register({required:true})}/>
+                            I'm want personalized service.
+                        </label>
+                    </div>
+                    <div class="col">
+                        <label class="radio-inline" for="interest-3">
+                            <input type="radio" name="interest" id="interest-3" value="just curious" ref={register({required:true})}/>
+                            I'm just curious.
+                        </label>
+                    </div>
+                </div>
+            </div>
+        
+
+            <div class="form-group">
+                <div class="row">
+                    <label class="col control-label" for="contactMethod">How would you like to be contacted?</label>
+                    <div class="col" id="contactMethod_error">
+                        <label class="radio-inline" for="contactMethod-email">
+                            <input type="radio" name="contactMethod" id="contactMethod-email" value="email"
+                                ref={register({required:true})}/>
+                            Email &nbsp;&nbsp;
+                        </label>
+                        <label class="radio-inline" for="contactMethod-phone">
+                            <input type="radio" name="contactMethod" id="contactMethod-phone" value="phone"
+                                ref={register({required:true})}/>
+                            Phone Call
+                        </label>
+                    </div>
+                </div>
+            </div>
+        
+
+            <div class="form-group" id="phoneGroup">
+                <div class="row">
+                    <label class="col control-label" for="phone">Phone Number</label>
+                    <div class="col">
+                        <input id="phone" name="phone" type="text" placeholder="702-555-5555" class="form-control input-md"
+                            data-parsley-pattern="^[+]?[1]?[-.\s]?[(]?[2-9]\d{2}[)]?[-.\s]?\d{3}[-.\s]?\d{4}$"
+                            ref={register({required:true})}/>
+                    </div>
+                </div>
+            </div>
+        
+
+            <div class="form-group form-group-last">
+                <div class="row">
+                    <div class="col text-center">
+                        <button id="submit" name="submit" class="btn btn-primary " value="validate">Get a Quote!</button>
+                    </div>
+                </div>
+            </div>
+        
+        <div class="loading js-loading hidden">
+            <div class="loading-spinner">
+                <svg>
+                    <circle cx="25" cy="25" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /></svg>
+            </div>
+        </div>
+    </form>
     )
 }
