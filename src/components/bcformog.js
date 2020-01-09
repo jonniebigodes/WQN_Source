aa9a512cdc7cfb7234607fd37424041f1b6419d0
Doomd <bcosteloe@gmail.com>
2020-01-08 23:00:22 -0700

    Revert "Changed the Script Address in Google Scripts to see if I can get sendmail to work on new version"
    This reverts commit fa0f8af3df6bf7d22247c4f290bd001b9cf51df2.

--- src/components/bcformog.js
+++ src/components/bcformog.js
@@ -2,8 +2,12 @@ import React from 'react'
 import useForm from "react-hook-form"
 
 export default function App(props) {
+<<<<<<< HEAD:src/components/bcformog.js
     //const scriptURL = 'https://script.google.com/macros/s/AKfycbwTay7c2eGmweCIRfj6lzMloYt7oH0toBcueXRVnQ/exec' //Production URL
     const scriptURL = "https://script.google.com/macros/s/AKfycbzpbG1CcPH5y7BGW6cJ5r2VivimxL7EQl96RBx8Cp6qRj1MW7zm/exec" //Test URL https://docs.google.com/spreadsheets/d/1CoQ2ZOVJLT9U9OkgdEvxuX3vNg-wZwLjbOEYr0Ivfhc/edit#gid=0
+=======
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwbrIjWVJfPDC4AZGHmopV3sDXDRvrZ7BniEVP2shUn0EjJDFV9/exec'
+>>>>>>> parent of fa0f8af... Changed the Script Address in Google Scripts to see if I can get sendmail to work on new version:src/components/bcformfuck.js
     const { register, handleSubmit, errors } = useForm({
         mode: "onBlur"
     });
